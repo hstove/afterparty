e9386b2d5f99472287cff47c10e0457253ec12a6
Hank Stoever <hstove@gmail.com>
2013-05-14 08:44:16 -0700

    initialize class variable @@redis with local connection

--- lib/afterparty.rb
+++ lib/afterparty.rb
@@ -1,10 +1,13 @@
 require 'logger'
 require 'afterparty/queue_helpers'
 require 'afterparty/redis_queue'
+require 'redis'
 Dir[File.expand_path('../afterparty/*', __FILE__)].each { |f| require f }
 
 
 module Afterparty
+  @@redis = Redis.new
+
   def self.redis
     @@redis
   end
