d3842c0e78ae63a396c7b7a82d9ed871fac67886
Hank Stoever <hstove@gmail.com>
2013-05-20 13:31:30 -0700

    make travis pass

--- spec/afterparty_job_spec.rb
+++ spec/afterparty_job_spec.rb
@@ -10,7 +10,7 @@ describe AfterpartyJob do
     job = AfterpartyJob.make_with_job tester
     job.reload
     (reloaded = job.reify).class.should == tester.class
-    reloaded.execute_at.should == tester.execute_at
+    reloaded.execute_at.utc.should == tester.execute_at.utc
     job.execute_at.should == reloaded.execute_at
   end
 
