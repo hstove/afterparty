8872e177717fcacd77e7b2d9d2e0df18449b7209
Hank Stoever <hstove@gmail.com>
2013-11-01 09:03:50 -0700

    DRYs up helper jobs

--- lib/afterparty/jobs.rb
+++ lib/afterparty/jobs.rb
@@ -1,41 +1,37 @@
 module Afterparty
-  class MailerJob
-    attr_accessor :execute_at, :mail, :clazz, :method, :args
-    def initialize clazz, method, *args
-      # @mail = UserMailer.welcome_email(User.find(1))
-      @clazz = clazz
-      @method = method
-      @args = args
-    end
-    
-    def run
-      @mail = @clazz.send @method, *@args
-      @mail.deliver
-    end
 
-    def description
-      desc = "Mailer: #{(@clazz || "nil")}."
-      desc << "Method: #{(@method || nil)}."
-      desc << "Args: #{(@args || nil)}"
-    end
-  end
+  module JobDescribers
+    attr_accessor :clazz, :method, :args, :execute_at
 
-  class BasicJob
-    attr_accessor :object, :method, :args
     def initialize object, method, *args
       @object = object
       @method = method
       @args = args
     end
 
+    def description
+      desc = "Mailer: #{(@object || "nil")}."
+      desc << "Method: #{(@method || "nil")}."
+      desc << "Args: #{(@args || "nil")}"
+    end
+    alias_method :inspect, :description
+  end
+
+  class MailerJob
+    include JobDescribers
+
+    def run
+      @mail = @object.send @method, *@args
+      @mail.deliver
+    end
+  end
+
+  class BasicJob
+    include JobDescribers
+
     def run
       @object.send(@method, *@args)
     end
-
-    def description
-      desc = "Object: #{(@object || "nil")}."
-      desc << "Method: #{(@method || nil)}."
-      desc << "Args: #{(@args || nil)}"
-    end
   end
+
 end
